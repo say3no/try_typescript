ce98dd334de1083f63fd64ed3a67aabdfaabe69c
say3no <say3no@gmail.com>
2018-11-11 00:10:57 +0900

    poy poy

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,41 @@
 module.exports = {
-  entry: "./src/Hello.tsx",
-  output: {
-    filename: "bundle.js",
-    path: __dirname + "/dist"
-  },
-  externals: {
-    "react": "React",
-    "react-dom": "ReactDOM",
-  },
+    entry: "./src/Hello.tsx",
+    output: {
+        filename: "bundle.js",
+        path: __dirname + "/dist"
+    },
+    /*
+    externals: {
+      "react": "React",
+      "react-dom": "ReactDOM",
+    },
+    */
+    // デバッグ用にソースマップの出力を有効にします。
+    devtool: "source-map",
 
-  // デバッグ用にソースマップの出力を有効にします。
-  devtool: "source-map",
+    resolve: {
+        // 解決可能な拡張子として、'.ts' と '.tsx' を追加します。
+        extensions: [".ts", ".tsx", ".js", ".json"]
+    },
 
-  resolve: {
-    // 解決可能な拡張子として、'.ts' と '.tsx' を追加します。
-    extensions: [".ts", ".tsx", ".js", ".json"]
-  },
+    module: {
+        rules: [
+            // 拡張子が .ts と .tsx　のファイル を 'awesome-typescript-loader' で
+            // 扱うようにします。
+            {
+                test: /\.tsx?$/,
+                loader: "awesome-typescript-loader"
+            },
 
-  module: {
-    rules: [
-      // 拡張子が .ts と .tsx　のファイル を 'awesome-typescript-loader' で
-      // 扱うようにします。
-      { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
-
-      // 出力されるすべての .js ファイルは、'source-map-loader' で
-      // 再加工されたソースマップを持ちます。
-      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
-    ]
-  },
+            // 出力されるすべての .js ファイルは、'source-map-loader' で
+            // 再加工されたソースマップを持ちます。
+            {
+                enforce: "pre",
+                test: /\.js$/,
+                loader: "source-map-loader"
+            }
+        ]
+    },
 
 
 };
